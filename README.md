5e6732b474df50c38b4582ea0b48e8268b199cd9
Dashan Gao <dashan.gao@connect.ust.hk>
2020-02-21 20:20:42 +0800

    add process_audio.py and README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+# Audio_To_Text
---
+++ process_audio.py
@@ -0,0 +1,32 @@
+from pydub import AudioSegment
+import os
+
+source_file = "/Users/gds/Documents/Code/speech_Baidu/speech-demo/rest-api-asr/python/audio/full_talk_1h.mp3"
+output_dir = "output_audios/"
+
+
+def process_audio(audio_file, output_dir="output_audios/"):
+    slice_length = 60 * 1000  # 60s per each file
+    overlap = 0.5 * 1000  # 0.5s overlap between short audios.
+    begin = 0
+    index = 0
+    # Make output dir
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+
+    source_file_format = audio_file.split(".")[-1]
+    audio = AudioSegment.from_file(audio_file, format=source_file_format)
+    # Split and process source audio file.
+    while begin < len(audio):
+        output_audio = audio[int(begin): int(begin+slice_length)]  #
+        seconds = begin / 1000
+        output_file_name = output_dir + "{0:04d}_{minus}m{second}s.wav".format(index, minus=int(seconds/60),
+                                                                               second=int(seconds) % 60)
+        output_audio = output_audio.set_channels(1).set_frame_rate(16000)  # change to 1 channel and 16k bitrate
+        output_audio.export(output_file_name, format="wav")  # save audio file.
+        begin += slice_length - overlap
+        index += 1
+
+
+if __name__ == '__main__':
+    process_audio(source_file, output_dir)
